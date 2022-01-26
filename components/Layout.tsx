d1d51a8b70a725ccd0cd855ff87911ec11f0b408
Luka Hartwig <mail@lukahartwig.de>
2022-01-27 00:08:39 +0100

    initial commit

---
+++ components/Layout.tsx
@@ -0,0 +1,15 @@
+import { ReactNode } from "react";
+
+interface Props {
+  children: ReactNode;
+}
+
+export default function Layout({ children }: Props) {
+  return (
+    <>
+      <div className="h-screen bg-rose-800">
+        <div className="container mx-auto h-full">{children}</div>
+      </div>
+    </>
+  );
+}
