226fbc6846a38dd271791bab6c99c30fab65a276
Luka Hartwig <mail@lukahartwig.de>
2022-06-18 15:48:44 +0200

    chore: update next config

--- next.config.js
+++ next.config.js
@@ -4,7 +4,13 @@
  * @type {import('next').NextConfig}
  **/
 const nextConfig = {
+  compiler: {
+    reactRemoveProperties: true,
+    removeConsole: true,
+  },
+  poweredByHeader: false,
   swcMinify: true,
+  reactStrictMode: true,
 };
 
 module.exports = nextConfig;
